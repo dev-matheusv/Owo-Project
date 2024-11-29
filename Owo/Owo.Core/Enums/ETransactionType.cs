48170d96106a36e9bdbe39fce489c3333b5e4092
Matheus Vinicius <107558422+dev-matheusv@users.noreply.github.com>
2024-11-28 23:46:14 -0300

    Add files via upload
    Estruturando o projeto e criando a documentação da API com Swagger

---
+++ Owo/Owo.Core/Enums/ETransactionType.cs
@@ -0,0 +1,7 @@
+namespace Owo.Core.Enums;
+
+public enum ETransactionType
+{
+    Deposit = 1,
+    Withdraw = 2
+}
---
+++ Owo/Owo.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace Owo.Core.Models;
+
+public class Category
+{
+    public long Id { get; set; }
+    public String Title { get; set; } = string.Empty;
+    public String Description { get; set; } = string.Empty;
+    public String UserId { get; set; } = string.Empty;
+}
