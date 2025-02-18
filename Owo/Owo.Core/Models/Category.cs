17dfdecac7c627f3b2c38d1b4e9b7a31dd498b07
Matheus Vinicius <devmatheusoxs@gmail.com>
2025-02-18 19:38:44 -0300

    Commitando alguns ajustes no repositório e também a referência do Owo.Core

--- Owo/Owo.Core/Models/Category.cs
+++ Owo/Owo.Core/Models/Category.cs
@@ -4,6 +4,6 @@ public class Category
 {
     public long Id { get; set; }
     public String Title { get; set; } = string.Empty;
-    public String Description { get; set; } = string.Empty;
+    public String? Description { get; set; }
     public String UserId { get; set; } = string.Empty;
 }
